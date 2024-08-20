9cc9b32613ddc82d1299637804802027ecafb277
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-20 18:51:48 +0500

    fix: post

--- apps/users/views.py
+++ apps/users/views.py
@@ -9,3 +9,12 @@ User = get_user_model()
 class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = SignupSerializer
+
+    def post(self, request, *args, **kwargs):
+
+        print(request.data)
+
+        print(request.data.get("email"))
+        print(request.data.get("password"))
+
+        return self.create(request, *args, **kwargs)
