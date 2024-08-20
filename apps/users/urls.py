3e5f26dd2d4403030114f7e01ab63b6498d66da6
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-20 16:34:33 +0500

    feat: login singup backend

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.users.views import SignupView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("signup/", SignupView.as_view(), name="signup"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from apps.users.serializers import SignupSerializer
+
+User = get_user_model()
+
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
