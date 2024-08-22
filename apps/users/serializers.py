15076c8e350cfe5f0a7053a38bf442c4127305ea
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-22 18:42:47 +0500

    fix: signup serializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -7,19 +7,13 @@ User = get_user_model()
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            "email",
-            "password",
-        )
-        extra_kwargs = {
-            "password": {"write_only": True},
-        }
+        fields = ("email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         try:
             user = User.objects.create_user(
-                email=validated_data["email"],
-                password=validated_data["password"],
+                email=validated_data["email"], password=validated_data["password"]
             )
             return user
         except Exception as e:
