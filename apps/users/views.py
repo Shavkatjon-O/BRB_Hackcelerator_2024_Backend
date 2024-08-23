f76daf083eecee3e3abd0e3deaa180d556d6e484
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 16:03:07 +0500

    fix: backend

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 from apps.users.serializers import SignupSerializer, UserSerializer
 
+from rest_framework.response import Response
+from rest_framework import status
+
 User = get_user_model()
 
 
@@ -20,3 +23,12 @@ class UserDetailView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    def put(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response(serializer.data)
+        else:
+            print("Validation Errors:", serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
