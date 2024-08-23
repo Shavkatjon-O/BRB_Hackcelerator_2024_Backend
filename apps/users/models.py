0537e49a92916dd65b319693c5bcf0fe871bd314
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-23 15:40:17 +0500

    fix: user profile

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,10 +7,22 @@ from apps.users.managers import UserManager
 
 class User(AbstractUser, BaseModel):
     username = None
-    first_name = None
-    last_name = None
 
     email = models.EmailField(unique=True)
+
+    first_name = models.CharField(max_length=126, null=True, blank=True)
+    last_name = models.CharField(max_length=126, null=True, blank=True)
+    phone_number = models.CharField(max_length=32, null=True, blank=True)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+
+    job_title = models.CharField(max_length=126, null=True, blank=True)
+    department = models.CharField(max_length=126, null=True, blank=True)
+    education = models.CharField(max_length=256, null=True, blank=True)
+
+    employment_start_date = models.DateField(null=True, blank=True)
+    skills = models.TextField(null=True, blank=True)
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = "email"
