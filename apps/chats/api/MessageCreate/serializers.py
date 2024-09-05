87a58881e9e426896782c35841a8bfedb6ee554c
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 00:08:26 +0500

    fix: backend

--- apps/chats/api/MessageCreate/serializers.py
+++ apps/chats/api/MessageCreate/serializers.py
@@ -2,9 +2,16 @@ from rest_framework import serializers
 from apps.chats.models import Message
 
 
-class MessageCreateSerializer(serializers.Serializer):
+class MessageCreateSerializer(serializers.ModelSerializer):
     text = serializers.CharField(max_length=1000)
     chat_id = serializers.IntegerField()
+
+    class Meta:
+        model = Message
+        fields = (
+            "text",
+            "chat_id",
+        )
 
 
 __all__ = ("MessageCreateSerializer",)
