1135d53785780fa48a28ddfa614ed33c368b6479
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-05 11:42:50 +0500

    fix: consumers in chats websockets integartion

--- apps/chats/consumers.py
+++ apps/chats/consumers.py
@@ -1,67 +1,62 @@
 import json
-from django.contrib.auth import get_user_model
 from channels.generic.websocket import AsyncWebsocketConsumer
 from apps.chats.models import Chat, Message
-
-
-User = get_user_model()
+from apps.chats.serializers import MessageSerializer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
-        self.chat_group_name = f"chat_{self.chat_id}"
+        self.room_group_name = f"chat_{self.chat_id}"
 
         # Join room group
         await self.channel_layer.group_add(
-            self.chat_group_name,
+            self.room_group_name,
             self.channel_name,
         )
+
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
-            self.chat_group_name,
+            self.room_group_name,
             self.channel_name,
         )
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-
         message_text = text_data_json["text"]
         user_id = self.scope["user"].id
 
-        chat = await Chat.objects.get(id=self.chat_id)
-        user = await User.objects.get(id=user_id)
-
-        # Save message to the database
+        # Save message to database
+        chat = Chat.objects.get(id=self.chat_id)
         message = Message.objects.create(
-            user=user,
             chat=chat,
+            user_id=user_id,
             text=message_text,
         )
 
+        # Serialize message
+        message_serializer = MessageSerializer(message)
+
         # Send message to room group
         await self.channel_layer.group_send(
-            self.chat_group_name,
+            self.room_group_name,
             {
                 "type": "chat_message",
-                "message": message_text,
-                "user": user.username,
+                "message": message_serializer.data,
             },
         )
 
     async def chat_message(self, event):
         message = event["message"]
-        user = event["user"]
 
         # Send message to WebSocket
         await self.send(
             text_data=json.dumps(
                 {
                     "message": message,
-                    "user": user,
                 },
             ),
         )
--- core/asgi.py
+++ core/asgi.py
@@ -1,14 +1,18 @@
+import os
+import environ
+
+from django.urls import path
+from django.core.asgi import get_asgi_application
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+
+from apps.chats.consumers import ChatConsumer
+
 import django
 
 django.setup()
-
-import os
-import environ
-from django.urls import path
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from apps.chats.consumers import ChatConsumer
 
 env = environ.Env()
 env.read_env(".env")
@@ -16,12 +20,14 @@ env.read_env(".env")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                [
-                    path("ws/chat/<int:chat_id>/", ChatConsumer.as_asgi()),
-                ]
-            )
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    [
+                        path("ws/chats/<int:chat_id>/", ChatConsumer.as_asgi()),
+                    ],
+                ),
+            ),
         ),
-    }
+    },
 )
