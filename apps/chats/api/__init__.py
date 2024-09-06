240a3cbb8e50c2b3baccc70496b61a0d6db791a1
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 08:51:30 +0500

    fix: group chat endpoints

--- apps/chats/api/__init__.py
+++ apps/chats/api/__init__.py
@@ -4,7 +4,8 @@ from .DirectChatCreate import *
 from .DirectChatDetail import *
 from .DirectChatList import *
 
-# from .GroupChatDetail import *
-# from .GroupChatList import *
+from .GroupChatDetail import *
+from .GroupChatList import *
+
 # from .MessageCreate import *
 # from .MessageList import *
