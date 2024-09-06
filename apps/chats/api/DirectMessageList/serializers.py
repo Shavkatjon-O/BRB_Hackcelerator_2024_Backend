aa24cb15b0a485ee5b0fb58cbd766d0fad737fff
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-06 13:31:34 +0500

    feat: chat message list backend

--- apps/chats/api/DirectMessageList/serializers.py
+++ apps/chats/api/DirectMessageList/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from apps.chats.models import DirectMessage
 
+from apps.chats.api.UserList.serializers import UserListSerializer
+from apps.chats.api.DirectChatList.serializers import DirectChatListSerializer
+
 
 class DirectMessageListSerializer(serializers.ModelSerializer):
+    user = UserListSerializer()
+    chat = DirectChatListSerializer()
+
     class Meta:
         model = DirectMessage
         fields = (
