055394b7319fc5b8761d2ca5fb58048ccfa50cf3
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-09 03:06:57 +0500

    fix(settings): update allowed hosts, cors, csrf trusted origins

--- core/settings/production.py
+++ core/settings/production.py
@@ -3,11 +3,13 @@ from core.settings.base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "admin.brb-titans.uz",
+    ".brb-titans.uz",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://www.brb-titans.uz",
     "https://admin.brb-titans.uz",
+    "wss://www.brb-titans.uz",
     "wss://admin.brb-titans.uz",
 ]
 CSRF_COOKIE_SECURE = True
@@ -28,6 +30,7 @@ LOGGER_CHAT_ID = env.str("LOGGER_CHAT_ID")
 
 CORS_ALLOWED_ORIGINS = [
     "https://www.brb-titans.uz",
+    "https://admin.brb-titans.uz",
 ]
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
