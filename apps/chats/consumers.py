d8d87bb35d15fd01c8a41fcb42e2d8e21451d3aa
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-09 01:09:01 +0500

    fix: backend

--- apps/chats/consumers.py
+++ apps/chats/consumers.py
@@ -1,5 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -8,62 +11,61 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.chat_group_name = f"chat_{self.chat_id}"
 
         # Join room group
-        await self.channel_layer.group_add(
-            self.chat_group_name,
-            self.channel_name,
-        )
-
+        await self.channel_layer.group_add(self.chat_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
-            self.chat_group_name,
-            self.channel_name,
-        )
+        await self.channel_layer.group_discard(self.chat_group_name, self.channel_name)
 
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message_data = text_data_json.get("message", {})
+        try:
+            text_data_json = json.loads(text_data)
+            message_data = text_data_json.get("message", {})
 
-        # Ensure message includes the required fields
-        user_data = message_data.get("user", {})
-        chat_data = message_data.get("chat", {})
+            user_data = message_data.get("user", {})
+            chat_data = message_data.get("chat", {})
 
-        # Broadcast message to the group
-        await self.channel_layer.group_send(
-            self.chat_group_name,
-            {
-                "type": "chat_message",
-                "id": message_data.get("id"),  # If applicable, include message ID
-                "user": {
-                    "email": user_data.get("email", ""),
-                    "first_name": user_data.get("first_name", ""),
-                    "last_name": user_data.get("last_name", ""),
-                    "image": user_data.get("image", ""),
-                },
-                "chat": {
-                    "id": chat_data.get("id", ""),
-                    "partner": {
-                        "email": chat_data.get("partner", {}).get("email", ""),
-                        "first_name": chat_data.get("partner", {}).get(
-                            "first_name", ""
-                        ),
-                        "last_name": chat_data.get("partner", {}).get("last_name", ""),
-                        "image": chat_data.get("partner", {}).get("image", ""),
+            # Broadcast message to the group
+            await self.channel_layer.group_send(
+                self.chat_group_name,
+                {
+                    "type": "chat_message",
+                    "id": message_data.get("id"),
+                    "user": {
+                        "email": user_data.get("email", ""),
+                        "first_name": user_data.get("first_name", ""),
+                        "last_name": user_data.get("last_name", ""),
+                        "image": user_data.get("image", ""),
                     },
+                    "chat": {
+                        "id": chat_data.get("id", ""),
+                        "partner": {
+                            "email": chat_data.get("partner", {}).get("email", ""),
+                            "first_name": chat_data.get("partner", {}).get(
+                                "first_name", ""
+                            ),
+                            "last_name": chat_data.get("partner", {}).get(
+                                "last_name", ""
+                            ),
+                            "image": chat_data.get("partner", {}).get("image", ""),
+                        },
+                    },
+                    "text": message_data.get("text", ""),
+                    "created_at": message_data.get("created_at", ""),
                 },
-                "text": message_data.get("text", ""),
-                "created_at": message_data.get("created_at", ""),
-            },
-        )
+            )
+        except json.JSONDecodeError:
+            logger.error("Error decoding WebSocket message")
+        except Exception as e:
+            logger.error(f"Error processing WebSocket message: {e}")
 
     async def chat_message(self, event):
         # Send the event back to the WebSocket
         await self.send(
             text_data=json.dumps(
                 {
-                    "id": event.get("id"),  # Ensure ID is included if applicable
+                    "id": event.get("id"),
                     "user": event.get("user"),
                     "chat": event.get("chat"),
                     "text": event.get("text"),
--- apps/chats/signals.py
+++ apps/chats/signals.py
@@ -5,43 +5,49 @@ from channels.layers import get_channel_layer
 from .models import DirectMessage
 
 
+def get_chat_partner(instance):
+    """Helper function to get the chat partner."""
+    return (
+        instance.chat.user1
+        if instance.user == instance.chat.user2
+        else instance.chat.user2
+    )
+
+
 @receiver(post_save, sender=DirectMessage)
 def notify_new_message(sender, instance, created, **kwargs):
     if created:
         channel_layer = get_channel_layer()
+        partner = get_chat_partner(instance)
 
-        partner = (
-            instance.chat.user1
-            if instance.user == instance.chat.user2
-            else instance.chat.user2
-        )
-
-        print(partner)
-
-        # Ensure that the message follows the same structure as the WebSocket consumer
-        async_to_sync(channel_layer.group_send)(
-            f"chat_{instance.chat.id}",
-            {
-                "type": "chat_message",  # This triggers the 'chat_message' method in the consumer
-                "id": instance.id,  # Include message ID if applicable
-                "text": instance.text,
-                "created_at": (
-                    instance.created_at.isoformat() if instance.created_at else ""
-                ),
-                "user": {
-                    "email": instance.user.email,
-                    "first_name": instance.user.first_name,
-                    "last_name": instance.user.last_name,
-                    "image": (instance.user.image.url if instance.user.image else None),
-                },
-                "chat": {
-                    "id": instance.chat.id,
-                    "partner": {
-                        "email": partner.email,
-                        "first_name": partner.first_name,
-                        "last_name": partner.last_name,
-                        "image": (partner.image.url if partner.image else None),
+        try:
+            async_to_sync(channel_layer.group_send)(
+                f"chat_{instance.chat.id}",
+                {
+                    "type": "chat_message",
+                    "id": instance.id,
+                    "text": instance.text,
+                    "created_at": (
+                        instance.created_at.isoformat() if instance.created_at else ""
+                    ),
+                    "user": {
+                        "email": instance.user.email,
+                        "first_name": instance.user.first_name,
+                        "last_name": instance.user.last_name,
+                        "image": (
+                            instance.user.image.url if instance.user.image else None
+                        ),
+                    },
+                    "chat": {
+                        "id": instance.chat.id,
+                        "partner": {
+                            "email": partner.email,
+                            "first_name": partner.first_name,
+                            "last_name": partner.last_name,
+                            "image": partner.image.url if partner.image else None,
+                        },
                     },
                 },
-            },
-        )
+            )
+        except Exception as e:
+            print(f"Error sending WebSocket message: {e}")
