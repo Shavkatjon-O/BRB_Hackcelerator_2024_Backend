276c8cf4abf2144f306902ea05ab815e3e17cfe2
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-11 04:09:52 +0500

    feat: rest framework pagination class

--- core/settings/base.py
+++ core/settings/base.py
@@ -135,6 +135,8 @@ JAZZMIN_SETTINGS = {
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
