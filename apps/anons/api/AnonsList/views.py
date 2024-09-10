f6cc1504721a763d270c9896bb16aad88fd1e3ea
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-11 04:21:47 +0500

    feat: add pagination to anons list

--- apps/anons/api/AnonsList/views.py
+++ apps/anons/api/AnonsList/views.py
@@ -1,14 +1,22 @@
-from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from apps.anons.api.AnonsList.serializers import AnonsListSerializer
 from apps.anons.models import Anons
+
+
+class AnonsListPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 1000
 
 
 class AnonsListView(ListAPIView):
     serializer_class = AnonsListSerializer
     permission_classes = [IsAuthenticated]
     queryset = Anons.objects.all()
+    pagination_class = AnonsListPagination
 
 
 __all__ = ("AnonsListView",)
