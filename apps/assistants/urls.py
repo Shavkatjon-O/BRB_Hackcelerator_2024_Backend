f39d614a0fe22e837814a08bf77919677b307d20
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-12 22:49:26 +0500

    fix

--- apps/assistants/urls.py
+++ apps/assistants/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import ChatView
 
 urlpatterns = [
-    path("chat/", ChatView.as_view(), name="chat"),
+    path("chat-support/", ChatView.as_view(), name="chat"),
 ]
