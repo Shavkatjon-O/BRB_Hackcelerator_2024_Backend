d5aff90afbe1fe9e2fa2332766a4a29c47080f81
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-15 16:28:00 +0500

    fix

--- apps/meetings/models.py
+++ apps/meetings/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+from apps.common.models import BaseModel
+from apps.users.models import User
+
+
+class Meeting(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
