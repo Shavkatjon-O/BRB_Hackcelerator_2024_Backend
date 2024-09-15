ff9dcfea303641fb0be87c4193136279c752b332
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-15 16:32:55 +0500

    fix: views

--- apps/meetings/views.py
+++ apps/meetings/views.py
@@ -7,7 +7,13 @@ class MeetingCreateView(generics.CreateAPIView):
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class MeetingListView(generics.ListAPIView):
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
+
+    def get_queryset(self):
+        return Meeting.objects.filter(user=self.request.user)
