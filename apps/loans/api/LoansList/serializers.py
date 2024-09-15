d9c84257d9b0cd3845ed6a4c9a98eed8c117a99d
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-16 01:02:26 +0500

    fix

--- apps/loans/api/LoansList/serializers.py
+++ apps/loans/api/LoansList/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from apps.loans.models import Loan
 
+from apps.clients.api.ClientsDetail.serializers import ClientDetailSerializer
+
 
 class LoansListSerializer(serializers.ModelSerializer):
+    client = ClientDetailSerializer()
+
     class Meta:
         model = Loan
         fields = "__all__"
